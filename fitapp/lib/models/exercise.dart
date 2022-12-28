c778b23c9132876932ecfffe37070762fe97e600
Pedro Henrique B. Peres <pedrohperescode@gmail.com>
2022-12-28 00:32:19 -0300

    exerciseModel

---
+++ fitapp/lib/models/exercise.dart
@@ -0,0 +1,13 @@
+class Exercise {
+
+  final String titulo;
+  final int? quantidade;
+  final DateTime? tempo;
+
+  Exercise({
+    required this.titulo,
+    this.quantidade,
+    this.tempo
+  });
+
+}
